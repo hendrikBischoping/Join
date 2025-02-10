41d9d10acce868775ec9d625d01989d2f1643018
Andreas Welker <andreaswelker@aol.com>
2025-02-11 00:41:00 +0100

    fix bug  contact-floating

--- src/app/main/contacts/contact-floating/contact-floating.component.ts
+++ src/app/main/contacts/contact-floating/contact-floating.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { IContact } from '../../../interfaces/icontact';
+import { ContactService } from '../../../services/contact-service.service';
+
 
 @Component({
   selector: 'app-contact-floating',
@@ -8,5 +11,26 @@ import { Component } from '@angular/core';
   styleUrl: './contact-floating.component.scss'
 })
 export class ContactFloatingComponent {
+ contactsFromList: IContact[]= [
+    {
+      name: "Hendrik Bischoping",
+      eMail: "hbischoping@googlemail.com",
+      phone: 31245,
+    },
+  ];
+
+  constructor(private contactService: ContactService) {
+    
+  }
+
+  ngOnInit() {
+    this.contactService.getContacts().subscribe((contactList) => {
+      this.contactsFromList = contactList;
+    })
+  }
+
+
+
+
 
 }
--- src/app/main/contacts/contacts.component.ts
+++ src/app/main/contacts/contacts.component.ts
@@ -1,20 +1,14 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { ContactFloatingComponent } from './contact-floating/contact-floating.component';
 
 
-=======
 import { ContactListComponent } from './contact-list/contact-list.component';
->>>>>>> fbfb272363ca49597e3520f272238ae783a576d1
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-<<<<<<< HEAD
-  imports: [ContactFloatingComponent],
-=======
-  imports: [ContactListComponent],
->>>>>>> fbfb272363ca49597e3520f272238ae783a576d1
+  imports: [ContactFloatingComponent, ContactListComponent],
+
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
