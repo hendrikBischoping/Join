01da1ece505ab0e9db62b7128b7bf1126b8076de
SasToepfer <sas.toepfer@gmail.com>
2025-02-10 11:49:18 +0100

    base structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,13 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"join-f8360","appId":"1:982335821033:web:9fc96c8f3a4f9ee8828c78","storageBucket":"join-f8360.firebasestorage.app","apiKey":"AIzaSyBc1im1vE_w_mPoyzTAPcrYa0u_Pq_ctwI","authDomain":"join-f8360.firebaseapp.com","messagingSenderId":"982335821033"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [
+    provideRouter(routes), 
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"join-f8360","appId":"1:982335821033:web:9fc96c8f3a4f9ee8828c78","storageBucket":"join-f8360.firebasestorage.app","apiKey":"AIzaSyBc1im1vE_w_mPoyzTAPcrYa0u_Pq_ctwI","authDomain":"join-f8360.firebaseapp.com","messagingSenderId":"982335821033"}))), 
+    importProvidersFrom(provideAuth(() => getAuth())), 
+    importProvidersFrom(provideFirestore(() => getFirestore())), provideAnimationsAsync()
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { SummaryComponent } from './main/summary/summary.component';
+import { BoardComponent } from './main/board/board.component';
+import { AddTaskComponent } from './main/add-task/add-task.component';
+import { ContactsComponent } from './main/contacts/contacts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'summary', component: SummaryComponent },
+    { path: 'board', component: BoardComponent },
+    { path: 'add-task', component: AddTaskComponent },
+    { path: 'contacts', component: ContactsComponent }
+];
