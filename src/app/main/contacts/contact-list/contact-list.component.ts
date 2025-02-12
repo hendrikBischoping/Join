e1d65c2c2932bed3f96864478cc5b09835a506f1
hendrikBischoping <h.bischoping@gmx.de>
2025-02-12 13:18:54 +0100

    added list-sorting and grouping by first letters

--- src/app/main/contacts/contact-list/contact-list.component.ts
+++ src/app/main/contacts/contact-list/contact-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ContactService } from '../../../services/contact-service.service';
 import { IContact } from '../../../interfaces/icontact';
+import { IsActiveMatchOptions } from '@angular/router';
 
 @Component({
   selector: 'app-contact-list',
@@ -10,26 +11,47 @@ import { IContact } from '../../../interfaces/icontact';
   styleUrl: './contact-list.component.scss'
 })
 export class ContactListComponent {
-  contactsFromList: IContact[]= [];
-
-
   
+  groupedContacts: {letter: string; contacts: IContact[]}[] = [];
+  
+  contactsFromList: IContact[] = [];  
 
   constructor(private contactService: ContactService) {
-    // this.sortContacts();
+    setTimeout(() => {
+      this.groupContacts();
+      console.log(this.contactsFromList);
+      
+    }, 1000);
   }
 
   ngOnInit() {
     this.contactService.getContacts().subscribe((contactList) => {
       this.contactsFromList = contactList;
-      this.sortContacts();
-    })
-  }
-
-  sortContacts() {
-    this.contactsFromList.sort((a, b) => a.name.localeCompare(b.name));
-    console.log(this.contactsFromList);
+    });
     
+
   }
 
+  // sortContacts() {
+  //   this.contactsFromList.sort((a, b) => a.name.localeCompare(b.name));
+  //   console.log(this.contactsFromList); 
+  // }
+
+  groupContacts() {
+    const sortedContacts = this.contactsFromList.sort((a, b) => a.name.localeCompare(b.name));
+
+    // 2. Erstelle eine Map mit Buchstaben als Schlüssel und Kontaktlisten als Werte
+    const groups = sortedContacts.reduce((acc, contact) => {
+      const letter = contact.name.charAt(0).toLowerCase(); // Ersten Buchstaben holen
+      if (!acc[letter]) {
+        acc[letter] = []; // Neue Gruppe erstellen, falls sie noch nicht existiert
+      }
+      acc[letter].push(contact); // Kontakt zur passenden Gruppe hinzufügen
+      return acc;
+    }, {} as {[key: string]: IContact[]});
+
+    this.groupedContacts = Object.keys(groups).sort().map(letter => ({
+      letter, contacts: groups[letter]
+    }));
+  }
 }
