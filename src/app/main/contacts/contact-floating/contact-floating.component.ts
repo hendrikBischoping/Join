59161346d43d04d5b8f3a88a87fae8d210440200
Andreas Welker <andreaswelker@aol.com>
2025-02-13 12:10:48 +0100

    make trash and edit1

--- src/app/main/contacts/contact-floating/contact-floating.component.ts
+++ src/app/main/contacts/contact-floating/contact-floating.component.ts
@@ -4,61 +4,55 @@ import { ContactService } from '../../../services/contact-service.service';
 import { EditContactDialogComponent } from '../edit-contact-dialog/edit-contact-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 
-
 @Component({
   selector: 'app-contact-floating',
   standalone: true,
   imports: [],
   templateUrl: './contact-floating.component.html',
-  styleUrl: './contact-floating.component.scss'
+  styleUrl: './contact-floating.component.scss',
 })
 export class ContactFloatingComponent {
- contactsFromList: IContact[]= [
+  contactsFromList: IContact[] = [
     {
-      name: "Hendrik Bischoping",
-      eMail: "hbischoping@googlemail.com",
+      name: 'Hendrik Bischoping',
+      eMail: 'hbischoping@googlemail.com',
       phone: 31245,
     },
   ];
   readonly dialog = inject(MatDialog);
 
-  constructor(private contactService: ContactService, private cdRef: ChangeDetectorRef) {
-    
-  }
+  constructor(
+    private contactService: ContactService,
+    private cdRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     this.contactService.getContacts().subscribe((contactList) => {
       this.contactsFromList = contactList;
-    })
+    });
   }
 
   editData(contactId?: string) {
-
-    const selectedContact = this. contactsFromList.find(contact => contact.id === contactId);
+    const selectedContact = this.contactsFromList.find(
+      (contact) => contact.id === contactId
+    );
     if (!selectedContact) {
-      console.error("Contact not found!");
+      console.error('Contact not found!');
       return;
     }
 
     const dialog = this.dialog.open(EditContactDialogComponent, {
-      data: { contact: { ...selectedContact } } // Kopie des Objekts übergeben
+      data: { contact: { ...selectedContact } }, // Kopie des Objekts übergeben
     });
-    
 
     dialog.afterClosed().subscribe((result) => {
       if (result) {
-        this.contactService.updateContact(contactId!, result); 
-        this.cdRef.detectChanges(); 
+        this.contactService.updateContact(contactId!, result);
+        this.cdRef.detectChanges();
       }
     });
   }
   deleteData(contactId?: string) {
-
-this.contactService.deleteContact(contactId!);
-
+    this.contactService.deleteContact(contactId!);
   }
-  
-
-
-
 }
