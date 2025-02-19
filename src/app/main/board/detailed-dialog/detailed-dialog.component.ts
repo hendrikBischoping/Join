0dea792580ca99c23254b3c04ddf86cf22097610
SasToepfer <sas.toepfer@gmail.com>
2025-02-19 15:59:00 +0100

    base layout detailed task overlay

--- src/app/main/board/detailed-dialog/detailed-dialog.component.ts
+++ src/app/main/board/detailed-dialog/detailed-dialog.component.ts
@@ -3,16 +3,18 @@ import { ITask } from '../../../interfaces/itask';
 import { IContact } from '../../../interfaces/icontact';
 import { TaskDataService } from '../../../services/task-data.service';
 import { ContactService } from '../../../services/contact-service.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detailed-dialog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './detailed-dialog.component.html',
   styleUrl: './detailed-dialog.component.scss'
 })
 export class DetailedDialogComponent {
   @Input() currentTaskId = "";
+  @Input() close!: () => void;
 
   task: ITask = {
       title: "Unknown",
@@ -26,6 +28,9 @@ export class DetailedDialogComponent {
       id: ""
     };
     contacts!: IContact[];
+
+    isUserStory = false;
+    prioImagePath = "";
   
     constructor(private taskDataService: TaskDataService, private contactService: ContactService) {
   
@@ -42,5 +47,31 @@ export class DetailedDialogComponent {
           }
         });
       });
+      this.initTask();
     }
+
+    initTask() {
+      if (this.task.category == "User Story") {
+        this.isUserStory = true;
+      }
+      this.prioImagePath = this.getPrioImagePath();
+    }
+
+    getPrioImagePath():string {
+      switch (this.task.priority) {
+        case "low": return "./assets/img/icons/TaskLowIcon.png";
+        case "mid": return "./assets/img/icons/TaskMediumIcon.png";
+        case "high": return "./assets/img/icons/TaskHighIcon.png";
+      }
+      return "";
+    }
+
+    editTask() {
+
+    }
+
+    deleteTask() {
+      
+    }
+
 }
