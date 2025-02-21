955d178fc057399501ba009456bceeaf963d0de0
SasToepfer <sas.toepfer@gmail.com>
2025-02-21 10:04:22 +0100

    polished Add Contacts to Task

--- src/app/main/board/detailed-dialog/detailed-dialog.component.ts
+++ src/app/main/board/detailed-dialog/detailed-dialog.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, HostListener, Input } from '@angular/core';
 import { ITask } from '../../../interfaces/itask';
 import { IContact } from '../../../interfaces/icontact';
 import { TaskDataService } from '../../../services/task-data.service';
@@ -7,7 +7,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-detailed-dialog',
@@ -21,113 +21,134 @@ export class DetailedDialogComponent {
   @Input() close!: () => void;
 
   task: ITask = {
-      title: "Unknown",
-      description: "No task found",
-      contacts: [],
-      date: 0,
-      priority: "mid",
-      category: "Userstory",
-      subtasks: [{
-        subtaskName: "",
-        subtaskDone: false,
-      }
-    ],
-      status: "todo",
-      id: ""
-    };
-    contacts: IContact[] = [];
+    title: "Unknown",
+    description: "No task found",
+    contacts: [],
+    date: 0,
+    priority: "mid",
+    category: "Userstory",
+    subtasks: [{
+      subtaskName: "",
+      subtaskDone: false,
+    }],
+    status: "todo",
+    id: ""
+  };
+  previewTask: ITask = {
+    title: "",
+    description: "",
+    contacts: [],
+    date: 0,
+    priority: "mid",
+    category: "",
+    subtasks: [],
+    status: "",
+    id: ""
+  };
+  contacts: IContact[] = [];
 
-    isUserStory = false;
-    editView = true;
-    prioImagePath = "";
-    dropdownOpen = false;
-    searchTerm: string = "";
-    filteredContacts : IContact[] = [];
+  isUserStory = false;
+  editView = true;
+  prioImagePath = "";
+  dropdownOpen = false;
+  searchTerm: string = "";
+  filteredContacts: IContact[] = [];
 
-    constructor(private taskDataService: TaskDataService, private contactService: ContactService, public cdRef: ChangeDetectorRef) {
-  
-    }
-    
-  
-    ngOnInit() {
-      this.contactService.getContacts().subscribe((contactList) => {
-        this.contacts = contactList;
-        this.filteredContacts = [...this.contacts];
-        this.cdRef.detectChanges();
-      });
-      this.taskDataService.getTasks().subscribe((taskList) => {
-        taskList.forEach(element => {
-          if (this.currentTaskId == element.id) {
-            this.task = element;
-          }
-        });
-      });
-      this.initTask();
-      
-    }
 
-    initTask() {
-      if (this.task.category == "User Story") {
-        this.isUserStory = true;
-      }
-      this.prioImagePath = this.getPrioImagePath();
-    }
+  constructor(private taskDataService: TaskDataService, private contactService: ContactService, public cdRef: ChangeDetectorRef) {
 
-    filterContacts(event: any) {
-      const query = event.target.value.toLowerCase();
-      this.filteredContacts = this.contacts.filter(contact =>
-        contact.name.toLowerCase().includes(query)
-      );
-  
-      console.log("Filtered Contacts:", this.filteredContacts); // Debugging
+  }
+
+
+  ngOnInit() {
+    this.contactService.getContacts().subscribe((contactList) => {
+      this.contacts = contactList;
+      this.filteredContacts = [...this.contacts];
       this.cdRef.detectChanges();
-    }
+    });
+    this.taskDataService.getTasks().subscribe((taskList) => {
+      taskList.forEach(element => {
+        if (this.currentTaskId == element.id) {
+          this.task = element;
+          this.previewTask = JSON.parse(JSON.stringify(this.task));
+        }
+      });
+    });
+    this.initTask();
 
-    getPrioImagePath():string {
-      switch (this.task.priority) {
-        case "low": return "./assets/img/icons/TaskLowIcon.png";
-        case "mid": return "./assets/img/icons/TaskMediumIcon.png";
-        case "high": return "./assets/img/icons/TaskHighIcon.png";
-      }
-      return "";
-    }
+  }
 
-    editTask(inEditMode = false) {
-      this.taskDataService.updateTask(this.currentTaskId, this.task)
-      
-      if (inEditMode) {
-        this.toggleEditMode();
-      }
+  initTask() {
+    if (this.task.category == "User Story") {
+      this.isUserStory = true;
     }
+    this.prioImagePath = this.getPrioImagePath();
+  }
 
-    deleteTask() {
-      
-    }
+  filterContacts(event: any) {
+    const query = event.target.value.toLowerCase();
+    this.filteredContacts = this.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(query)
+    );
+    this.cdRef.detectChanges();
+  }
 
-    toggleEditMode() {
-      this.editView = !this.editView;
-      this.cdRef.detectChanges();
+  getPrioImagePath(): string {
+    switch (this.task.priority) {
+      case "low": return "./assets/img/icons/TaskLowIcon.png";
+      case "mid": return "./assets/img/icons/TaskMediumIcon.png";
+      case "high": return "./assets/img/icons/TaskHighIcon.png";
     }
+    return "";
+  }
 
-    openDropdown() {
-      this.dropdownOpen = true;
-      this.cdRef.detectChanges(); // Manuelles Aktualisieren der Ansicht
+  editTask(inEditMode = false) {
+    if (inEditMode) {
+      Object.assign(this.task, this.previewTask);
+      this.toggleEditMode();
     }
-  
-    closeDropdown() {
-      setTimeout(() => { 
-        this.dropdownOpen = false;
-        this.cdRef.detectChanges();
-      }, 200); // Kleines Delay, damit Klicks noch registriert werden
-    }
+    this.taskDataService.updateTask(this.currentTaskId, this.task)
+    this.cdRef.detectChanges();
+  }
 
-    toggleContactAssignment(contactId: string) {
-      const index = this.task.contacts!.indexOf(contactId);
-      if (index === -1) {
-        this.task.contacts!.push(contactId); // Falls der Kontakt nicht drin ist → hinzufügen
-      } else {
-        this.task.contacts!.splice(index, 1); // Falls der Kontakt drin ist → entfernen
-      }
-      this.cdRef.markForCheck();
+  deleteTask() {
+
+  }
+
+  toggleEditMode() {
+    this.editView = !this.editView;
+    this.cdRef.detectChanges();
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    if (!this.isClickInsideDropdown(event)) {
+      this.closeDropdown();
     }
+  }
+
+  openDropdown(event: Event) {
+    event.stopPropagation();
+    this.dropdownOpen = true;
+    this.cdRef.detectChanges(); 
+  }
+
+  closeDropdown() {
+    this.dropdownOpen = false;
+    this.cdRef.detectChanges();
+  }
+
+  isClickInsideDropdown(event: Event): boolean {
+    const dropdownContainer = document.querySelector(".assignment-container");
+    return dropdownContainer?.contains(event.target as Node) ?? false;
+  }
+
+  toggleContactAssignment(contactId: string) {
+    const index = this.previewTask.contacts!.indexOf(contactId);
+    if (index === -1) {
+      this.previewTask.contacts!.push(contactId); 
+    } else {
+      this.previewTask.contacts!.splice(index, 1);
+    }
+  }
 }
